fcb6c3882bc52fdd65494b4d2728831075a72a38
Jackson Myhre <jack421myhre@gmail.com>
2022-10-06 16:18:31 -0500

    Modified the models and their associations. Will have to test to make sure everything is being targeted correctly.

--- models/index.js
+++ models/index.js
@@ -7,7 +7,13 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category);
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
